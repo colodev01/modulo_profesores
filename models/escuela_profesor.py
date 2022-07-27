7cc1d8bd4978ba433c11d44ead6d14020db4b017
franco <francomuzzin01@gmail.com>
2022-07-27 11:40:40 -0300

    Adding autoincrement legajo and new empty views

--- models/escuela_profesor.py
+++ models/escuela_profesor.py
@@ -8,15 +8,31 @@ class EscuelaProfesor(models.Model):
 
     nombre = fields.Char(string="Profesor", default="Jose Perez")
     edad = fields.Integer()
-    legajo = fields.Char("Legajo")
+    legajo = fields.Integer()
 
     materia_id = fields.Many2many(
         comodel_name="escuela.materia", string="Materia"
     )
 
-    # Implementar en metodo create() auto-incremento de variable legajo
+    def getHighestLegajo(self):
+        profesores = self.env["escuela.profesor"].search([])
+        legajoAlto=0
+        if len(profesores)>0:
+            for i in range(len(profesores)):
+                if legajoAlto < profesores[i].legajo:
+                    legajoAlto = profesores[i].legajo
+        return legajoAlto+1
+
+    @api.model
+    def create(self, values):
+        values['legajo'] = self.getHighestLegajo()
+        record = super(EscuelaProfesor, self).create(values)
+        return record
+
+    """
     @api.constrains("legajo")
     def check_legajo(self):
         legajos = self.search([]).mapped("legajo")
         if self.legajo and self.legajo in legajos:
             raise exceptions.ValidationError("Existe legajo doble")
+    """
